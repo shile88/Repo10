8a805fb76b6caf57cec4172578c0191ce560e965
shile88 <milos.ivanis@gmail.com>
2022-04-11 15:19:21 +0200

    hasOwnProperty

---
+++ ex09/src/index.js
@@ -0,0 +1,19 @@
+var myObj = {
+    title: "Titanic",
+    song: "My Heart Will Go On",
+    genre: "drama"
+}
+
+function myFunction(myObj, checkProp){
+    if (myObj.hasOwnProperty(checkProp)){
+        return myObj[checkProp];
+    }
+    return "Not found";   
+}
+
+console.log(myFunction(myObj, "title"));
+console.log(myFunction(myObj, "song"));
+console.log(myFunction(myObj, "genre"));
+console.log(myFunction(myObj, "actor"));
+
+module.exports = myFunction;
