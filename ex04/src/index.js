4399e3f3fa23c22aecb508aa381220ede2ac8014
shile88 <milos.ivanis@gmail.com>
2022-04-11 12:45:59 +0200

    storing object prop

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+var players ={
+    7: "Muhamed Besic",
+    10: "Miralem Pjanic",
+    11: "Edin Dzeko"
+};
+
+function myFunction(players){
+    var playerNumber = 10 ;
+    var player = players[playerNumber];
+    return player;
+}
+
+console.log(myFunction(players));
+module.exports = {players, myFunction};
